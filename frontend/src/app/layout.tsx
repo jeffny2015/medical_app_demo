2cbbf141f72864dd00e72d3d3aa50808f48c50c9
ocreamer888 <macm1air@Ocreamer-3.local>
2025-05-20 11:03:48 -0600

    WIP: guardando cambios antes del pull

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en"> 
       <body className={`${inter.className} flex flex-col min-h-screen`}>
         <Header />
         <main className="pt-8 flex-grow flex items-center justify-center">
